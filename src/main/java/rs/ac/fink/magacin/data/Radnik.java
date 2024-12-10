1b4f05c7977b9625966b15238f800be90848d1fd
andje <andjelacorbic2001@gmail.com>
2024-12-10 16:33:59 +0100

    klasa radnik

---
+++ src/main/java/rs/ac/fink/magacin/data/Radnik.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rs.ac.fink.magacin.data;
+/**
+ *
+ * @author andje
+ */
+public class Radnik {
+   
+    private int id;
+
+    private String ime;
+    private String prezime;
+    private String username;
+    private String telefon;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+}
