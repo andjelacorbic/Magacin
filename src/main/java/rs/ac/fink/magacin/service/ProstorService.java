92d863d0b0f33986a61bdaa03114222a7b36ab38
andje <andjelacorbic2001@gmail.com>
2024-12-11 11:32:18 +0100

    dodavanje klasa proizvod i prostor u data sloju

--- src/main/java/rs/ac/fink/magacin/service/ProstorService.java
+++ src/main/java/rs/ac/fink/magacin/service/ProstorService.java
@@ -8,6 +8,90 @@ package rs.ac.fink.magacin.service;
  *
  * @author andje
  */
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import rs.ac.fink.magacin.dao.ProstorDao;
+import rs.ac.fink.magacin.dao.ResourcesManager;
+import rs.ac.fink.magacin.data.Prostor;
+import rs.ac.fink.magacin.exception.MagacinException;
+
 public class ProstorService {
-    
+
+    private static final ProstorService instance = new ProstorService();
+
+    private ProstorService() {
+    }
+
+    public static ProstorService getInstance() {
+        return instance;
+    }
+
+    public void addNewProstor(Prostor prostor) throws MagacinException {
+        Connection con = null;
+        try {
+            con = ResourcesManager.getConnection();
+            con.setAutoCommit(false);
+
+            ProstorDao.getInstance().insert(prostor, con);
+
+            con.commit();
+        } catch (SQLException ex) {
+            ResourcesManager.rollbackTransactions(con);
+            throw new MagacinException("Nije moguce dodati novi prostor " + prostor, ex);
+        } finally {
+            ResourcesManager.closeConnection(con);
+        }
+    }
+
+    public Prostor findProstor(int prostorId) throws MagacinException {
+        Connection con = null;
+        try {
+            con = ResourcesManager.getConnection();
+
+            return ProstorDao.getInstance().find(prostorId, con);
+        } catch (SQLException ex) {
+            throw new MagacinException("Nije moguce pronaci prostor koji zelite. " + prostorId, ex);
+        } finally {
+            ResourcesManager.closeConnection(con);
+        }
+    }
+
+    public void deleteProstor(int prostorId) throws MagacinException {
+        Connection con = null;
+        try {
+            con = ResourcesManager.getConnection();
+            con.setAutoCommit(false);
+
+            Prostor prostor = ProstorDao.getInstance().find(prostorId, con);
+            if (prostor != null) {
+                ProstorDao.getInstance().delete(prostorId, con);
+            }
+
+            con.commit();
+        } catch (SQLException ex) {
+            ResourcesManager.rollbackTransactions(con);
+            throw new MagacinException("Nije moguce obrisati prostor " + prostorId, ex);
+        } finally {
+            ResourcesManager.closeConnection(con);
+        }
+    }
+
+    public void updateProstor(Prostor prostor) throws MagacinException {
+        Connection con = null;
+        try {
+            con = ResourcesManager.getConnection();
+            con.setAutoCommit(false);
+
+            ProstorDao.getInstance().update(prostor, con);
+
+            con.commit();
+        } catch (SQLException ex) {
+            ResourcesManager.rollbackTransactions(con);
+            throw new MagacinException("Neuspecna izmena. " + prostor, ex);
+        } finally {
+            ResourcesManager.closeConnection(con);
+        }
+    }
 }
+
